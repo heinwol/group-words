9635719512e36d13229ef77c5ca6ed33c75c4dec
heinwol <heinvvol@gmail.com>
2023-11-11 16:56:19 +0300

    formatted

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 #![allow(dead_code, unused_variables, unused_imports)]
 
-use std::{iter::once, fmt::{self, Formatter, Display, Debug}, hash::Hash, ops::Mul, };
+use itertools::{iproduct, Itertools};
 use std::collections::HashSet;
-use itertools::{Itertools, iproduct};
+use std::{
+    fmt::{self, Debug, Display, Formatter},
+    hash::Hash,
+    iter::once,
+    ops::Mul,
+};
 
 /// Splits string `s` at indices, right inclusive, like:
 /// (aaab^-12ca^4b^3, [3, 9, 12]) ->
@@ -30,11 +35,13 @@ pub struct Word {
 }
 
 impl Word {
-
     pub fn new(elts: &[WordLetter]) -> Self {
-        Word { elements: Vec::from(elts) }.free_reduction()
+        Word {
+            elements: Vec::from(elts),
+        }
+        .free_reduction()
     }
-    
+
     pub fn from_singleton(chr: char) -> Self {
         Word::from(&chr.to_string()[..])
     }
@@ -85,34 +92,44 @@ impl Word {
     }
 
     pub fn to_string_with_separator(&self, sep: &str) -> String {
-        let s: Vec<_> = self.elements.iter().map(
-            |(chr, pow)| {
-                if *pow != 1 { format!("{chr}^{pow}") }
-                else         { format!("{chr}") }
-            }
-        ).collect();
+        let s: Vec<_> = self
+            .elements
+            .iter()
+            .map(|(chr, pow)| {
+                if *pow != 1 {
+                    format!("{chr}^{pow}")
+                } else {
+                    format!("{chr}")
+                }
+            })
+            .collect();
         s.join(sep)
     }
 
     pub fn literal_form(&self) -> Vec<WordLetter> {
-        self.elements.iter().map(
-            |(chr, pow)| {
-                (0..pow.abs()).map(
-                    |i| (*chr, if *pow >= 0 {1i32} else {-1i32}))
-            }
-        ).flatten().collect()
+        self.elements
+            .iter()
+            .map(|(chr, pow)| (0..pow.abs()).map(|i| (*chr, if *pow >= 0 { 1i32 } else { -1i32 })))
+            .flatten()
+            .collect()
     }
 
     pub fn inv(&self) -> Self {
         Self {
-            elements: self.elements.iter().rev().map(
-                |(chr, pow)| { (*chr, -*pow) }
-            ).collect()
+            elements: self
+                .elements
+                .iter()
+                .rev()
+                .map(|(chr, pow)| (*chr, -*pow))
+                .collect(),
         }
     }
 
     pub fn symbols(&self) -> HashSet<char> {
-        self.elements.iter().map(|(chr, pow)| *chr).collect::<HashSet<char>>()
+        self.elements
+            .iter()
+            .map(|(chr, pow)| *chr)
+            .collect::<HashSet<char>>()
     }
 }
 
@@ -125,39 +142,30 @@ impl Clone for Word {
 }
 
 impl From<&str> for Word {
-
     fn from(s: &str) -> Self {
-
         let mut s_mut: String = s.into();
         // first, remove all whitespaces
         s_mut.retain(|chr| !chr.is_whitespace());
-        let int_res: Vec<_> =
-            s_mut.match_indices("^")
-                 .map(|(i, _)| i - 1)
-                 .collect();
+        let int_res: Vec<_> = s_mut.match_indices("^").map(|(i, _)| i - 1).collect();
         let s_split = split_at_indices(&s_mut, &int_res);
 
-        let maped: Vec<_> =
-            s_split.iter()
-                   .map(|s: &String| {
-                       s.as_str()
-                        .rfind(|c: char| c.is_ascii_digit())
-                        .map_or_else(
-                            || s.chars()
-                                .map(|x| {(x, 1i32)})
-                                .collect::<Vec::<WordLetter>>(),
-                            |idx| {
-                                let (l, r) = s.split_at(idx + 1);
-                                let left_char = l.chars().nth(0).unwrap();
-                                let left_power: i32 = (&l[2..]).to_string().parse().unwrap();
-                                let left_res = (left_char, left_power);
-                                let right_res = r.chars().map(|x| {(x, 1i32)});
-                                let res: Vec<_> = once(left_res).chain(right_res).collect();
-                                res
-                            }
-                        )
-                        
-                   }).collect();
+        let maped: Vec<_> = s_split
+            .iter()
+            .map(|s: &String| {
+                s.as_str().rfind(|c: char| c.is_ascii_digit()).map_or_else(
+                    || s.chars().map(|x| (x, 1i32)).collect::<Vec<WordLetter>>(),
+                    |idx| {
+                        let (l, r) = s.split_at(idx + 1);
+                        let left_char = l.chars().nth(0).unwrap();
+                        let left_power: i32 = (&l[2..]).to_string().parse().unwrap();
+                        let left_res = (left_char, left_power);
+                        let right_res = r.chars().map(|x| (x, 1i32));
+                        let res: Vec<_> = once(left_res).chain(right_res).collect();
+                        res
+                    },
+                )
+            })
+            .collect();
         let flattened = maped.concat();
         let res = Word::new(&flattened);
         res
@@ -174,14 +182,21 @@ impl Display for Word {
 impl Debug for Word {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         // let s = self.to_string_with_separator(" * ");
-        let s = self.elements.iter().map(
-            |(chr, pow)| 
-            if *pow != 1 { format!("{chr}^{pow}") }
-            else         { format!("{chr}") }
-        ).collect::<Vec<_>>().join(" ");
+        let s = self
+            .elements
+            .iter()
+            .map(|(chr, pow)| {
+                if *pow != 1 {
+                    format!("{chr}^{pow}")
+                } else {
+                    format!("{chr}")
+                }
+            })
+            .collect::<Vec<_>>()
+            .join(" ");
         if self.is_empty() {
             write!(f, "0")
-        } else{
+        } else {
             write!(f, "{s}")
         }
     }
@@ -207,20 +222,22 @@ impl Mul for &Word {
     }
 }
 
-
 pub fn cyclically_permute_vec<T>(v: &Vec<T>, shift: usize) -> Vec<T>
-where T: Clone {
+where
+    T: Clone,
+{
     let n = v.len();
     assert!(shift < n);
     let mut res = Vec::<T>::with_capacity(n);
-    res.extend_from_slice(&v[n-shift..n]);
-    res.extend_from_slice(&v[0..n-shift]);
+    res.extend_from_slice(&v[n - shift..n]);
+    res.extend_from_slice(&v[0..n - shift]);
     res
 }
 
-pub fn all_cyclic_permutations<T>(v: &Vec<T>) ->
-impl Iterator<Item = Vec<T>> + '_
-where T: Clone {
+pub fn all_cyclic_permutations<T>(v: &Vec<T>) -> impl Iterator<Item = Vec<T>> + '_
+where
+    T: Clone,
+{
     let n = v.len();
     (0..n).map(move |i| cyclically_permute_vec(&v, i))
 }
@@ -261,22 +278,23 @@ pub fn symmetrize(r: &HashSet<Word>) -> HashSet<Word> {
 }
 
 pub fn pieces(r: &HashSet<Word>, r_is_symmetrized: bool) -> HashSet<Word> {
-    let mut pcs = r.iter()
-                   .flat_map(|w| w.symbols())
-                   .collect::<HashSet<char>>()
-                   .iter()
-                   .flat_map(|chr| 
-                        [Word::new(&[(*chr, 1)]), 
-                         Word::new(&[(*chr, -1)])])
-                   .collect::<HashSet<Word>>();
-    
+    let mut pcs = r
+        .iter()
+        .flat_map(|w| w.symbols())
+        .collect::<HashSet<char>>()
+        .iter()
+        .flat_map(|chr| [Word::new(&[(*chr, 1)]), Word::new(&[(*chr, -1)])])
+        .collect::<HashSet<Word>>();
+
     let temp;
-    let r_sym = if !r_is_symmetrized
-               { temp = symmetrize(r);
-                 &temp }
-                else { r };
+    let r_sym = if !r_is_symmetrized {
+        temp = symmetrize(r);
+        &temp
+    } else {
+        r
+    };
     let r_list: Vec<&Word> = r_sym.iter().collect();
-    for i in 0..r_list.len(){
+    for i in 0..r_list.len() {
         for j in 0..i {
             pcs.insert(common_piece(r_list[i], r_list[j]));
         }
@@ -285,34 +303,33 @@ pub fn pieces(r: &HashSet<Word>, r_is_symmetrized: bool) -> HashSet<Word> {
     pcs
 }
 
-pub fn maximum_word_partitioning(pcs: &HashSet<Word>, w: &Word) ->
-    Vec<Word> {
-        let literal_form = w.literal_form();
-        if literal_form.len() < 2 {
-            return literal_form.iter().map(
-                |(chr, pow)| Word::new(
-                    &Vec::from([(*chr, *pow)])
-                )
-            ).collect();
-        }
-        let mut subpartition = Vec::<Word>::with_capacity(w.elements.capacity());
-        let mut j = 0;
-        for i in 1..(literal_form.len() + 1) {
-            let check = Word::new(&literal_form[j..i]);
-            if !pcs.contains(&check) {
-                let to_add = Word::new(&literal_form[j..(i-1)]);
-                subpartition.push(to_add);
-                j = i-1;
-            }
-        }
-        subpartition.push(Word::new(&literal_form[j..literal_form.len()]));
-        subpartition
-        // TODO add assertions
+pub fn maximum_word_partitioning(pcs: &HashSet<Word>, w: &Word) -> Vec<Word> {
+    let literal_form = w.literal_form();
+    if literal_form.len() < 2 {
+        return literal_form
+            .iter()
+            .map(|(chr, pow)| Word::new(&Vec::from([(*chr, *pow)])))
+            .collect();
     }
-
+    let mut subpartition = Vec::<Word>::with_capacity(w.elements.capacity());
+    let mut j = 0;
+    for i in 1..(literal_form.len() + 1) {
+        let check = Word::new(&literal_form[j..i]);
+        if !pcs.contains(&check) {
+            let to_add = Word::new(&literal_form[j..(i - 1)]);
+            subpartition.push(to_add);
+            j = i - 1;
+        }
+    }
+    subpartition.push(Word::new(&literal_form[j..literal_form.len()]));
+    subpartition
+    // TODO add assertions
+}
 
 pub fn all_c6_single_relator_groups<T>(seq: T) -> HashSet<Word>
-where T: Iterator<Item = Word> {
+where
+    T: Iterator<Item = Word>,
+{
     let mut res = HashSet::<Word>::new();
     for relator in seq {
         let r = HashSet::<Word>::from([relator]);
@@ -332,23 +349,24 @@ where T: Iterator<Item = Word> {
 }
 
 pub fn multiply_many(seq: &Vec<&Word>) -> Word {
-        let res_len = seq.iter().map(|w| w.len()).sum();
-        let mut res_vec = Vec::<WordLetter>::with_capacity(res_len);
-        for v in seq.iter() {
-            res_vec.extend(v.elements.iter())
-        }
-        Word::new(&res_vec)
+    let res_len = seq.iter().map(|w| w.len()).sum();
+    let mut res_vec = Vec::<WordLetter>::with_capacity(res_len);
+    for v in seq.iter() {
+        res_vec.extend(v.elements.iter())
+    }
+    Word::new(&res_vec)
 }
 
 fn iterate_sth() {
     let vars = vec!['a', 'b'];
-    let vars_word = vars.iter().map(
-        |chr| Word::from_singleton(*chr)
-    ).collect::<HashSet<Word>>();
+    let vars_word = vars
+        .iter()
+        .map(|chr| Word::from_singleton(*chr))
+        .collect::<HashSet<Word>>();
 
-    let letters_with_inverses = 
-        (&vars_word | &inv_set(&vars_word))
-            .into_iter().collect::<Vec<Word>>();
+    let letters_with_inverses = (&vars_word | &inv_set(&vars_word))
+        .into_iter()
+        .collect::<Vec<Word>>();
 
     let mut combinations = vec![letters_with_inverses; 6];
     combinations[0] = vec![Word::from("a")];
@@ -356,29 +374,22 @@ fn iterate_sth() {
     // println!("{:?}", combinations);
     let multiprod = combinations.iter().multi_cartesian_product();
 
-    let res = all_c6_single_relator_groups(
-        multiprod.map(|w_vec| 
-            multiply_many(&w_vec)
-        ));
+    let res = all_c6_single_relator_groups(multiprod.map(|w_vec| multiply_many(&w_vec)));
     println!("{:?}", res.len());
 }
 
 // ------------------------------------------------------------
 
 fn main() {
-
-
-
     // let w  = Word::from("a a a b^-1 a b b");
 
     // let w2 = Word::from("a a a b^-1 a b b");
     // let mut set = HashSet::<Word>::new();
     // println!("{:?}", set.insert(w.clone()));
     // println!("{:?}", set.contains(&w2));
-    
+
     // let w = Word::new(&vec![('a', 1), ('a', 1)]);
     // println!("{:?}", w.elements);
-
 
     // println!("{:?}", w);
     // let sym = symmetrize(&(vec![w.clone()].into_iter().collect()));
@@ -389,5 +400,4 @@ fn main() {
     // println!("{:?}", pcs.iter().map(|w| &w.elements).collect::<Vec<_>>());
 
     iterate_sth();
-
 }
